d72c68a83c0df009e7fce413637de29c0ec958ee
Derek Brown <djb3718@rit.edu>
2014-10-14 14:14:14 -0400

    Added branch to eclipse and created java file

---
+++ CSCI261_hw4_p1/src/LongestOscillatingSubseq.java
@@ -0,0 +1,15 @@
+/**
+ * LongestOscillatingSubseq.java
+ * 
+ * @author	Derek Brown <djb3718@rit.edu>
+ * 
+ * Purpose	Dynamic programming approach for finding the length of the longest
+ * 			subsequence of numbers whose values are oscillating
+ */
+
+public class LongestOscillatingSubseq {
+
+	public static void main(String[] args) {
+
+	}//end main
+}//end LongestOscillatingSubseq
